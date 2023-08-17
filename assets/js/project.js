d95c50873e814f04311975589deead8735dce010
AlexyJoaquim-Reino <98829232+AlexyJoaquim-Reino@users.noreply.github.com>
2023-08-17 18:58:51 +0200

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -64,7 +64,7 @@ const projects = [
   },
   
   {
-    title: "Projet personnel : Montage vidéo d'un jeu retro pixel d'aventure et d'horreur pour modifier son histoire (Projet en  cours de refonte)",
+    title: "Projet personnel en cours de refonte : Montage vidéo d'un jeu retro pixel d'aventure et d'horreur pour modifier son histoire",
     cardImage: "",
     description:
       "Un montage vidéo qui a permi d'imaginer une nouvelle histoire ou un autre point de vue d'une histoire d'un jeu vidéo retro-pixel, accompagné d'un long texte pour que le spectateur identifie parfaitement le point de vue du monteur vidéo.",
